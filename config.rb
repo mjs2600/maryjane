2334616d96d3a2b378632eb9acae9b38486f110a
Mike Simpson <mike.simpson@scimedsolutions.com>
2013-08-31 16:44:09 -0400

    Set relative links

--- config.rb
+++ config.rb
@@ -73,6 +73,7 @@ configure :build do
   # set :http_path, "/Content/images/"
 end
 
+set :relative_links, true
 activate :directory_indexes
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true
